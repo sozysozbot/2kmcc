0818c2e459e4966b85ed3e94e372a99819fbfad9
hsjoihs <jekto.vatimeliju@gmail.com>
2023-02-05 03:24:31 +0900

    Add some more incorrectly accepted ones

--- test_incorrectly_accepts.py
+++ test_incorrectly_accepts.py
@@ -2,12 +2,7 @@ from test import bcolors, should_not_compile
 
 print(f"{bcolors.WARNING}Checking the inputs that should NOT work, but is currently ACCEPTED:{bcolors.ENDC}")
 
-should_not_compile(
-    "struct A {int a;}; int main() { struct A a; a.b; return 0; }",
-    "cannot calculate the size for type `struct A`."
-)
-
-should_not_compile(
-    "struct A {int a;}; int main() { struct A a; return a.b; }",
-    "cannot calculate the size for type `struct A`."
-)
+should_not_compile("int main() {int i; int i; return 0;}")
+should_not_compile("struct A {int a;}; struct A {int b;}; int main() { return 0; }")
+should_not_compile("struct A {int a;}; int main() { struct A a; a.b; return 0; }")
+should_not_compile("struct A {int a;}; int main() { struct A a; return a.b; }")
