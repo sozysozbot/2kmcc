24cd5a844bcef981012fc934475d3c165bf0c6e3
hsjoihs <jekto.vatimeliju@gmail.com>
2023-02-05 03:17:17 +0900

    Add some more test cases

--- test_correctly_rejects.py
+++ test_correctly_rejects.py
@@ -30,8 +30,49 @@ def should_not_compile(input: str, expected_stderr: str = None):
 print(f"{bcolors.OKBLUE}Checking the inputs that should NOT work:{bcolors.ENDC}")
 
 assert should_not_compile(
+    "int main() { 3 5; }",
+    "parse error: expected an operator; got a number"
+)
+
+assert should_not_compile(
+    "int main() { 3 $ 5; }",
+    "unknown character $(36)"
+)
+
+assert should_not_compile(
+    "int main() { int a; return a.b; }",
+    "tried to access a member of a non-struct type"
+)
+
+assert should_not_compile(
+    "int main() { return sizeof(struct A); }",
+    "cannot calculate the size for type `struct A`."
+)
+
+assert should_not_compile(
+    "struct A {int a;}; int main() { struct A a; 3+a; return 0; }",
+    "invalid operands to binary `+`: types are `int` and `struct A`."
+)
+
+
+assert should_not_compile(
+    "struct A {int a;}; int main() { struct A a; 3 == a; return 0; }",
+    "invalid operands to binary `==`: types are `int` and `struct A`."
+)
+
+assert should_not_compile(
+    "struct A {int a;}; int main() { struct A a; a+3; return 0; }",
+    "invalid operands to binary `+`: types are `struct A` and `int`."
+)
+
+assert should_not_compile(
+    "struct A {int a;}; int main() { struct A a; a-3; return 0; }",
+    "invalid operands to binary `-`: types are `struct A` and `int`."
+)
+
+assert should_not_compile(
     "int main() { int x; *x; return 0; }",
-    "Cannot deref a non-pointer type"
+    "cannot deref a non-pointer type"
 )
 assert should_not_compile(
     "int main() { int x; int y; x = 3; y = &x; return *y; }",
@@ -39,7 +80,7 @@ assert should_not_compile(
 )
 assert should_not_compile(
     "int main() { int *p; char *q; return p-q;}",
-    "cannot subtract two expressions with different pointer types: `pointer to int` and `pointer to char`."
+    "invalid operands to binary `-`: types are `pointer to int` and `pointer to char`."
 )
 assert should_not_compile(
     "int main() { return p;}",
