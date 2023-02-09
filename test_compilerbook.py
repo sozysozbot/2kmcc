a9422297b1e057b97d178951fbc789d2de3b10b4
hsjoihs <jekto.vatimeliju@gmail.com>
2023-02-09 13:01:31 +0900

    step6 working

--- test_compilerbook.py
+++ test_compilerbook.py
@@ -578,3 +578,263 @@ assert check_stepN_that_2kmcc_compiled(5, step5, '(10+1)*2', 22)
 assert check_stepN_that_2kmcc_compiled(5, step5, '(10+1)/2', 5)
 assert check_stepN_that_2kmcc_compiled(5, step5, '(15+1)/2+3', 11)
 assert check_stepN_that_2kmcc_compiled(5, step5, '10+1 /2/5', 10)
+
+###################################################################################################
+print(f"{bcolors.OKBLUE}-------------------------------------------------{bcolors.ENDC}")
+print(f"{bcolors.OKBLUE}ステップ6：単項プラスと単項マイナス{bcolors.ENDC}")
+print(f"{bcolors.OKBLUE}Step 6: unary plus / unary minus{bcolors.ENDC}")
+###################################################################################################
+step6 = """int printf();
+char *strchr();
+void exit();
+void *calloc();
+
+struct Expr {
+    char binary_op;
+    char expr_kind;
+    int value;
+    struct Expr *first_child;
+    struct Expr *second_child;
+};
+
+struct Token {
+    char kind;
+    int value;
+};
+
+int isDigit(char c);
+int intLength(char *str);
+int parseInt(char *str);
+struct Expr *parseMultiplicative(struct Token **ptrptr, struct Token *token_end);
+struct Expr *parseAdditive(struct Token **ptrptr, struct Token *token_end);
+struct Expr *parseExpr(struct Token **ptrptr, struct Token *token_end);
+
+struct Token tokens[1000];
+
+void EvaluateExprIntoRax(struct Expr *expr) {
+    if (expr->expr_kind == '#') {
+        printf("  mov rax, %d\\n", expr->value);
+    } else if (expr->expr_kind == '@') {
+        EvaluateExprIntoRax(expr->first_child);
+        printf("    push rax\\n");
+        EvaluateExprIntoRax(expr->second_child);
+        printf("    push rax\\n");
+        printf("    pop rdi\\n");
+        printf("    pop rax\\n");
+        if (expr->binary_op == '+') {
+            printf("    add rax,rdi\\n");
+        } else if (expr->binary_op == '-') {
+            printf("    sub rax,rdi\\n");
+        } else if (expr->binary_op == '*') {
+            printf("    imul rax,rdi\\n");
+        } else if (expr->binary_op == '/') {
+            printf("  cqo\\n");
+            printf("  idiv rdi\\n");
+        } else {
+            exit(1);
+        }
+    } else {
+        exit(1);
+    }
+}
+
+struct Expr *numberexpr(int value) {
+    struct Expr *numberexp = calloc(1, sizeof(struct Expr));
+    numberexp->value = value;
+    numberexp->expr_kind = '#';
+    return numberexp;
+}
+
+struct Expr *binaryExpr(struct Expr *first_child, struct Expr *second_child, char binaryop) {
+    struct Expr *newexp = calloc(1, sizeof(struct Expr));
+    newexp->first_child = first_child;
+    newexp->expr_kind = '@';
+    newexp->binary_op = binaryop;
+    newexp->second_child = second_child;
+    return newexp;
+}
+
+struct Expr *parsePrimary(struct Token **ptrptr, struct Token *token_end) {
+    struct Token *maybe_number = *ptrptr;
+    if (maybe_number >= token_end) {
+        exit(1);
+    }
+    if (maybe_number->kind != '#') {
+        struct Token *maybe_leftparenthesis = maybe_number;
+        if (maybe_leftparenthesis->kind == '(') {
+            *ptrptr += 1;
+            struct Expr *expr = parseExpr(ptrptr, token_end);
+            struct Token *maybe_rightparenthesis = *ptrptr;
+            if (maybe_rightparenthesis->kind != ')') {
+                exit(1);
+            }
+            *ptrptr += 1;
+            return expr;
+        }
+        exit(1);
+    }
+    *ptrptr += 1;
+    return numberexpr(maybe_number->value);
+}
+
+struct Expr *parseUnary(struct Token **ptrptr, struct Token *token_end) {
+    struct Token *maybe_unary = *ptrptr;
+    if (maybe_unary >= token_end) {
+        exit(1);
+    }
+    if (maybe_unary->kind == '+') {
+        *ptrptr += 1;
+        return parsePrimary(ptrptr, token_end);
+    }
+    if (maybe_unary->kind == '-') {
+        *ptrptr += 1;
+        return binaryExpr(numberexpr(0), parsePrimary(ptrptr, token_end), '-');
+    }
+    return parsePrimary(ptrptr, token_end);
+}
+
+struct Expr *parseExpr(struct Token **ptrptr, struct Token *token_end) {
+    return parseAdditive(ptrptr, token_end);
+}
+
+struct Expr *parseMultiplicative(struct Token **ptrptr, struct Token *token_end) {
+    struct Token *tokens = *ptrptr;
+    if (token_end == tokens) {
+        exit(1);
+    }
+    struct Expr *result = parseUnary(&tokens, token_end);
+
+    for (; tokens < token_end;) {
+        struct Token maybe_operator = *tokens;
+        if (maybe_operator.kind == '#') {
+            exit(1);
+        } else if (maybe_operator.kind == '*') {
+            tokens++;
+            struct Expr *numberexp = parseUnary(&tokens, token_end);
+            result = binaryExpr(result, numberexp, '*');
+        } else if (maybe_operator.kind == '/') {
+            tokens++;
+            struct Expr *numberexp = parseUnary(&tokens, token_end);
+            result = binaryExpr(result, numberexp, '/');
+        } else {
+            *ptrptr = tokens;
+            return result;
+        }
+    }
+    *ptrptr = tokens;
+    return result;
+}
+
+struct Expr *parseAdditive(struct Token **ptrptr, struct Token *token_end) {
+    struct Token *tokens = *ptrptr;
+    if (token_end == tokens) {
+        exit(1);
+    }
+    struct Expr *result = parseMultiplicative(&tokens, token_end);
+
+    for (; tokens < token_end;) {
+        struct Token maybe_operator = *tokens;
+        if (maybe_operator.kind == '#') {
+            exit(1);
+        } else if (maybe_operator.kind == '-') {
+            tokens++;
+            struct Expr *numberexp = parseMultiplicative(&tokens, token_end);
+            result = binaryExpr(result, numberexp, '-');
+        }
+        if (maybe_operator.kind == '+') {
+            tokens++;
+            struct Expr *numberexp = parseMultiplicative(&tokens, token_end);
+            result = binaryExpr(result, numberexp, '+');
+        } else {
+            *ptrptr = tokens;
+            return result;
+        }
+    }
+    *ptrptr = tokens;
+    return result;
+}
+
+int tokenize(char *str) {
+    int token_index = 0;
+    for (int i = 0; str[i];) {
+        char c = str[i];
+        if (strchr("+()-*/", c)) {
+            tokens[token_index].kind = c;
+            token_index++;
+            i++;
+        } else if ('0' <= c && c <= '9') {
+            int parsednum = parseInt(&str[i]);
+            int parsedlength = intLength(&str[i]);
+            i += parsedlength;
+            tokens[token_index].kind = '#';
+            tokens[token_index].value = parsednum;
+            token_index++;
+        } else if (c == ' ') {
+            i++;
+        } else {
+            exit(1);
+        }
+    }
+    return token_index;
+}
+
+int parseInt(char *str) {
+    int result = 0;
+    while (isDigit(*str)) {
+        int digit = *str - '0';
+        result = result * 10 + digit;
+        str++;
+    }
+    return result;
+}
+int intLength(char *str) {
+    int length = 0;
+    while (isDigit(*str)) {
+        length++;
+        str++;
+    }
+    return length;
+}
+int isDigit(char c) {
+    return '0' <= c && c <= '9';
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        return 1;
+    }
+
+    char *p = argv[1];
+
+    int token_length = tokenize(p);
+
+    if (token_length == 0) {
+        return 1;
+    }
+
+    printf(".intel_syntax noprefix\\n");
+    printf(".globl main\\n");
+    printf("main:\\n");
+    struct Token *ptr = tokens;
+    struct Token *token_end = tokens + token_length;
+    struct Expr *expr = parseExpr(&ptr, token_end);
+    EvaluateExprIntoRax(expr);
+    printf("  ret\\n");
+    return 0;
+}"""
+
+assert check_stepN_that_2kmcc_compiled(6, step6, '0', 0)
+assert check_stepN_that_2kmcc_compiled(6, step6, '42', 42)
+assert check_stepN_that_2kmcc_compiled(6, step6, '0+10+3', 13)
+assert check_stepN_that_2kmcc_compiled(6, step6, '111+10-42', 79)
+assert check_stepN_that_2kmcc_compiled(6, step6, '   111   + 10 -     42', 79)
+assert check_stepN_that_2kmcc_compiled(6, step6, '   0 +    10+    3', 13)
+assert check_stepN_that_2kmcc_compiled(6, step6, '10*2', 20)
+assert check_stepN_that_2kmcc_compiled(6, step6, '10+1*2', 12)
+assert check_stepN_that_2kmcc_compiled(6, step6, '10+3*2+10-5', 21)
+assert check_stepN_that_2kmcc_compiled(6, step6, '(10+3)*2+10-5', 31)
+assert check_stepN_that_2kmcc_compiled(6, step6, '(10+1)*2', 22)
+assert check_stepN_that_2kmcc_compiled(6, step6, '(10+1)/2', 5)
+assert check_stepN_that_2kmcc_compiled(6, step6, '(15+1)/2+3', 11)
+assert check_stepN_that_2kmcc_compiled(6, step6, '10+1 /2/5', 10)
+assert check_stepN_that_2kmcc_compiled(6, step6, '-2*-3', 6)
